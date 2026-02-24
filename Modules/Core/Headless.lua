e30590da31c26d05d2b676440d770f3e333a1eef
LiTnTg4 <yuanming879@gmail.com>
2026-02-25 02:11:21 +0800

    更新 Headless.lua

--- Modules/Core/Headless.lua
+++ Modules/Core/Headless.lua
@@ -1,35 +1,37 @@
-local Headless={active=true,player=nil}
-local Finder=require(script.Parent.Parent.Utils.Finder)
+local Headless = {active = true, player = nil}
+
 function Headless.init(plr)
-    Headless.player=plr
+    Headless.player = plr
     task.spawn(function()
         while Headless.active do
             task.wait(1)
-            local c=Headless.player.Character
+            local c = Headless.player.Character
             if c then
-                local head=Finder.find(c,'Head')
-                if head and head.Transparency~=1 then
-                    head.Transparency=1
-                    head.CanCollide=false
-                    head.Massless=true
+                local head = _G.f(c, "Head")
+                if head and head.Transparency ~= 1 then
+                    head.Transparency = 1
+                    head.CanCollide = false
+                    head.Massless = true
                 end
-                local face=Finder.find(c,'Face')
-                if face then face:Destroy()end
+                local face = _G.f(c, "Face")
+                if face then face:Destroy() end
             end
         end
     end)
 end
+
 function Headless.enable(bool)
-    Headless.active=bool
-    local c=Headless.player.Character
+    Headless.active = bool
+    local c = Headless.player.Character
     if c then
-        local head=Finder.find(c,'Head')
+        local head = _G.f(c, "Head")
         if head then
-            head.Transparency=1
-            head.CanCollide=false
+            head.Transparency = 1
+            head.CanCollide = false
         end
-        local face=Finder.find(c,'Face')
-        if face then face:Destroy()end
+        local face = _G.f(c, "Face")
+        if face then face:Destroy() end
     end
 end
+
 return Headless
