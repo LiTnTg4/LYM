ee4e40667fc260e29fea24cd26bdd071b6625d90
LiTnTg4 <yuanming879@gmail.com>
2026-04-01 00:41:09 +0800

    更新 Headless.lua

--- Modules/Core/Headless.lua
+++ Modules/Core/Headless.lua
@@ -1,37 +1,36 @@
-local Headless = {active = true, player = nil}
+local Headless = {active = true}
 
-function Headless.init(plr)
-    Headless.player = plr
-    task.spawn(function()
-        while Headless.active do
-            task.wait(1)
-            local c = Headless.player.Character
-            if c then
-                local head = _G.f(c, "Head")
-                if head and head.Transparency ~= 1 then
-                    head.Transparency = 1
-                    head.CanCollide = false
-                    head.Massless = true
-                end
-                local face = _G.f(c, "Face")
-                if face then face:Destroy() end
-            end
-        end
-    end)
+function Headless.init(player)
+    Headless.player = player
 end
 
 function Headless.enable(bool)
     Headless.active = bool
     local c = Headless.player.Character
     if c then
-        local head = _G.f(c, "Head")
+        local head = c:FindFirstChild("Head")
         if head then
             head.Transparency = 1
             head.CanCollide = false
         end
-        local face = _G.f(c, "Face")
+        local face = c:FindFirstChild("Face")
         if face then face:Destroy() end
     end
+end
+
+function Headless.startLoop()
+    task.spawn(function()
+        while true do
+            task.wait(1)
+            local c = Headless.player.Character
+            if c then
+                local head = c:FindFirstChild("Head")
+                if head and head.Transparency ~= 1 then
+                    head.Transparency = 1
+                end
+            end
+        end
+    end)
 end
 
 return Headless
