21398bd09a7b5038e380ef2a2916961dfa09b5b4
LiTnTg4 <yuanming879@gmail.com>
2026-04-01 18:40:37 +0800

    创建 Headless.lua

---
+++ Modules/Core/Headless.lua
@@ -0,0 +1,35 @@
+local Headless = {}
+
+function Headless.init(player)
+    Headless.player = player
+end
+
+function Headless.enable(bool)
+    local c = Headless.player.Character
+    if c then
+        local head = c:FindFirstChild("Head")
+        if head then
+            head.Transparency = 1
+            head.CanCollide = false
+        end
+        local face = c:FindFirstChild("Face")
+        if face then face:Destroy() end
+    end
+end
+
+function Headless.startLoop(isUnloadedRef)
+    task.spawn(function()
+        while not isUnloadedRef() do
+            task.wait(1)
+            local c = Headless.player.Character
+            if c then
+                local head = c:FindFirstChild("Head")
+                if head and head.Transparency ~= 1 then
+                    head.Transparency = 1
+                end
+            end
+        end
+    end)
+end
+
+return Headless
